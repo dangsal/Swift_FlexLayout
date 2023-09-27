c1ec073db0389fdd1719c89f18df67db6f518f02
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-27 17:53:24 +0900

    [FEAT] 기본 사용법

--- Swift_FlexLayout/Swift_FlexLayout/FlexLayoutViewController.swift
+++ Swift_FlexLayout/Swift_FlexLayout/FlexLayoutViewController.swift
@@ -8,14 +8,57 @@
 import UIKit
 
 import FlexLayout
+import PinLayout
 
 final class FlexLayoutViewController: UIViewController {
     
+    private let flexView: UIView = UIView()
+    
+    private let label1: UILabel = {
+        let label = UILabel()
+        label.text = "label1"
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.numberOfLines = 1
+        return label
+    }()
+    private let label2: UILabel = {
+        let label = UILabel()
+        label.text = "label2"
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.numberOfLines = 1
+        return label
+    }()
+    private let label3: UILabel = {
+        let label = UILabel()
+        label.text = "label3"
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        label.numberOfLines = 1
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupLayout()
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        flexView.pin.all(view.pin.safeArea)
+        flexView.flex.layout()
+    }
+    
+    private func setupLayout() {
+        self.view.addSubview(self.flexView)
+        self.flexView.flex.define {
+            $0.addItem(self.label1)
+            $0.addItem(self.label2)
+            $0.addItem(self.label3)
+        }
+     
+        
+    }
 }
 
 #if DEBUG
